dda480773efc202004fe6e09d1b5e4ca13f41c02
Rafał Sumisławski <rafal.sumislawski@gmail.com>
2022-03-25 18:15:35 +0100

    Simplify IdGenerator (#89)

--- core/src/test/scala/net/sigusr/mqtt/impl/protocol/IdGeneratorSpec.scala
+++ core/src/test/scala/net/sigusr/mqtt/impl/protocol/IdGeneratorSpec.scala
@@ -1,29 +1,37 @@
 package net.sigusr.mqtt.impl.protocol
 
 import cats.effect.IO
-import cats.effect.testing.specs2.CatsIO
+import cats.effect.testing.specs2.CatsEffect
+import cats.syntax.all._
 import org.specs2.mutable._
 
-class IdGeneratorSpec extends Specification with CatsIO {
+// http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718025
+class IdGeneratorSpec extends Specification with CatsEffect {
+
+  val context = new net.sigusr.mqtt.SpecUtils.CatsContext
+  import context._
 
   "An id generator" should {
 
-    "Provide the next id" in {
-      IdGenerator[IO](41).map { g =>
-        for {
-          _ <- g.next
-          n <- g.next
-        } yield n must_== 42
-      }
+    "Provide ids > 0" in {
+      IdGenerator[IO]().flatMap(gen =>
+        gen.next.replicateA(100*1000)
+          .map(ids => ids.forall(_ > 0) must beTrue)
+      )
     }
 
-    "Provide ids modulus 65535" in {
-      IdGenerator[IO](65534).map { g =>
-        for {
-          _ <- g.next
-          n <- g.next
-        } yield n must_== 1
-      }
+    "Provide ids <= 65535" in {
+      IdGenerator[IO]().flatMap(gen =>
+        gen.next.replicateA(100*1000)
+          .map(ids => ids.forall(_ <= 65535) must beTrue)
+      )
+    }
+
+    "Provide 65535 different ids" in {
+      IdGenerator[IO]().flatMap(gen =>
+        gen.next.replicateA(65535)
+          .map(ids => ids.distinct.size must_== ids.size)
+      )
     }
   }
 }
